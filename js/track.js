45c7be21c6c6102fb7a5d0d5b4c5efcea7ee7fda
Kai <kai@uerlichs.de>
2020-12-14 20:49:29 +0100

    Fixed the issue

---
+++ js/track.js
@@ -0,0 +1,25 @@
+new Vue({
+  el: '#app',
+  data: {
+    track: [],
+    album: []
+  },
+  created() {
+
+    var vm = this
+
+    axios.get('https://www.theaudiodb.com/api/v1/json/1/track.php?h=32793500')
+      .then(function (response) {
+        vm.track = response.data.track[0]
+        console.log(vm.track)
+
+      })
+
+    axios.get('https://theaudiodb.com/api/v1/json/1/album.php?m=2115888')
+      .then(function (response) {
+        vm.album = response.data.album[0]
+        console.log(al.album)
+      })
+  }
+
+})
