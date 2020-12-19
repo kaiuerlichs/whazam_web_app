f4882ecbf1709e13e569a455d210a182ce164b9b
Kai <kai@uerlichs.de>
2020-12-19 13:47:53 +0100

    Fixed a bug and added index search functionality

--- js/search.js
+++ js/search.js
@@ -4,8 +4,24 @@ document.getElementById('navbarSearchButton').addEventListener("click", function
   var search = document.getElementById('navbarSearchInput');
   // inputbox value
   var toSearch = search.value;
-  // Adds the value to the url to be used in api call
-  let url = "'./artist.html?s=' +toSearch";
-  // changes the location to the url
-  window.location.assign("url")
+  if(toSearch !== ""){
+    // Adds the value to the url to be used in api call
+    let url = "/artist.html?s=" + toSearch;
+    // changes the location to the url
+    window.location.assign(url)
+  }
+})
+
+// gets the button id and adds event listener function that runs when clicked
+document.getElementById('indexSearchButton').addEventListener("click", function(){
+  // input box
+  var search = document.getElementById('indexSearchInput');
+  // inputbox value
+  var toSearch = search.value;
+  if(toSearch !== ""){
+    // Adds the value to the url to be used in api call
+    let url = "/artist.html?s=" + toSearch;
+    // changes the location to the url
+    window.location.assign(url)
+  }
 })
