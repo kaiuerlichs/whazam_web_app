6c0ff0aa48ab5f4ee51c41147821f60dd15f3e62
Kai Uerlichs <71931072+kaiuerlichs@users.noreply.github.com>
2020-12-20 21:55:34 +0100

    Oopsie, fixed the mobile serach

--- js/search.js
+++ js/search.js
@@ -13,7 +13,7 @@ document.getElementById('navbarSearchButtonDesktop').addEventListener("click", f
 // gets the button id and adds event listener function that runs when clicked
 document.getElementById('navbarSearchButtonMobile').addEventListener("click", function(){
   // inputbox value
-  var toSearch = document.getElementById('indexSearchInputMobile').value;
+  var toSearch = document.getElementById('navbarSearchInputMobile').value;
   if(toSearch !== ""){
     // Adds the value to the url to be used in api call
     let url = "artist.html?s=" + toSearch;
